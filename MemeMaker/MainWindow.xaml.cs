af85500ae45da24dbad995e53d4c2056872dc778
alumno <alumno@A301PC34>
2021-11-11 08:35:26 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemeMaker
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public object conversor(object value , Type targetType , object parameter , CultureInfo culture)
+        {
+            bool marcado = (bool)value;
+            if (marcado)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
